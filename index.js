ac995c079a83329ec4499fb74a41c42d11a5416a
ramirune <timpiga3@hotmail.com>
2022-01-24 20:41:59 +0100

    .

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ app.get(
 	passport.authenticate('jwt', { session: false }),
 	(req, res) => {
 		Users.findOne({ Username: req.params.Username })
-			.populate('FavoriteMovies', 'Title')
+
 			.then(user => {
 				res.status(201).json(user);
 			})
