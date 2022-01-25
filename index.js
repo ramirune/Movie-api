81f4dd158b25289afe9ba8185873b4ae0085f643
ramirune <timpiga3@hotmail.com>
2022-01-25 16:51:37 +0100

    .

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ app.get(
 	'movies/:MovieID',
 	passport.authenticate('jwt', { session: false }),
 	(req, res) => {
-		Movies.findOne({ _id: req.params.movie._id })
+		Movies.findOne({ Title: req.params._id })
 			.then(movie => {
 				res.status(201).json(movie);
 			})
